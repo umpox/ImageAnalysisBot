b46b853381ffd3ce96d034839280d774e8414b39
Tom Ross <tom@umpox.com>
2018-05-25 14:13:32 +0100

    linting

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 require('dotenv').config();
-const Unsplash = require('unsplash-js').default;
+require('isomorphic-fetch');
 
-const { 
-    UNSPLASH_APP_ID,
-    UNSPLASH_APP_SECRET,
-    UNSPLASH_APP_CALLBACK 
+const Unsplash = require('unsplash-js').default;
+const { toJson } = require('unsplash-js');
+
+const {
+  UNSPLASH_APP_ID,
+  UNSPLASH_APP_SECRET,
+  UNSPLASH_APP_CALLBACK,
 } = process.env;
 
 const unsplash = new Unsplash({
   applicationId: UNSPLASH_APP_ID,
   secret: UNSPLASH_APP_SECRET,
-  callbackUrl: UNSPLASH_APP_CALLBACK
+  callbackUrl: UNSPLASH_APP_CALLBACK,
 });
+
+unsplash.photos.getRandomPhoto()
+  .then(toJson)
+  .then(json => console.log(json));
